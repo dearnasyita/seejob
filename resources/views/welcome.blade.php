dfa6e20fcc44aa36ab27bcb3dfa1904e2b3bfa88
dearnasyita <dearnasyita01@gmail.com>
2019-05-06 20:42:34 +0700

    kedua

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -20,16 +20,16 @@
     <!-- Navbar -->
     <nav class="navbar navbar-expand-lg navbar-light ">
     <div class="container">
-        <a class="navbar-brand" href="#">SEE JOB</a>
+        <a class="navbar-brand" href="#">SEEJOB</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
         <div class="navbar-nav ml-auto">
-            <a class="nav-item nav-link active" href="#">Home <span class="sr-only">(current)</span></a>
-            <a class="nav-item nav-link" href="#">Features</a>
-            <a class="nav-item nav-link" href="#">About</a>
-            <a class="nav-item  btn btn-primary tombol" href="#">Join Us</a>
+            <a class="nav-item nav-link active" href="#">Trending <span class="sr-only">(current)</span></a>
+            <a class="nav-item nav-link" href="#">Lokasi</a>
+            <a class="nav-item nav-link" href="#">Perusahaan</a>
+            <a class="nav-item  btn btn-primary tombol" href="#">Masuk</a>
         </div>
         </div>
     </div>
@@ -39,7 +39,7 @@
     <!-- Jumbotron -->
     <div class="jumbotron jumbotron-fluid">
     <div class="container">
-        <h1 class="display-4">Get work done <span>faster</span><br> and <span>with</span> us </h1>
+        <h1 class="display-4"> <span>CARI APA YANG KAMU MAU</span><br> Kenali pekerjaan yang kamu impikan </h1>
     </div>
     </div>
     <!-- End Jumbotron -->
